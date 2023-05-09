962aeec7615c23f8309ba67885e5d1dc64ae08ef
Logan <ultimate360solutions@gmail.com>
2023-05-09 12:49:05 -0500

    added example startup script and Gnome app files

---
+++ begin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This is a shell script used to start the program, can be called on OS startup to auto start the program
+# Don't forget to make this file executable with sudo chmod +x begin.sh
+# Change the path below to the path of the program on your machine
+cd /home/logan/Daily-Programmer-Memes-Slideshow
+npm start
