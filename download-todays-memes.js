765c6ff2468f20f019b53347464b0f55f22bc80c
Logan <ultimate360solutions@gmail.com>
2023-05-10 16:38:52 -0500

    error handling for failed image downloads
    will now log error and retry download of image rather than skipping it

--- download-todays-memes.js
+++ download-todays-memes.js
@@ -41,38 +41,65 @@ axios.get(subredditUrl)
     downloadImages(imageUrls);
   })
   .catch(error => logToFile('Error fetching subreddit data: ' + error));
+  
+
+function downloadImage(url, index, retryCount = 0) {
+  const maxRetryCount = 5; // define the maximum number of retries
+  return axios.get(url, { responseType: 'arraybuffer' })
+    .then(response => {
+      const imageType = url.split('.').pop();
+      const fileName = `image-${index + 1}`;
+      const filePath = path.join(imagesFolderPath, fileName + '.' + imageType);
+
+      // Delete old file with the same name but different extension if exists
+      const extensions = ['jpg', 'png', 'gif'];
+      extensions.forEach(ext => {
+        if (ext !== imageType) {
+          const oldFilePath = path.join(imagesFolderPath, fileName + '.' + ext);
+          fs.unlink(oldFilePath, (err) => {
+            if (err && err.code !== 'ENOENT') {
+              // 'ENOENT' means file doesn't exist, ignore that error
+              logToFile('Error deleting old image file: ' + err);
+            }
+          });
+        }
+      });
+
+      // Write the new file
+      fs.writeFile(filePath, Buffer.from(response.data), (error) => {
+        if (error) {
+          logToFile('Error writing image file: ' + error);
+        } else {
+          logToFile(`Image ${index + 1} saved as ${fileName}.${imageType}`);
+        }
+      });
+    })
+    .catch(error => {
+      logToFile('Error downloading image: ' + error);
+      if (retryCount < maxRetryCount) {
+        logToFile(`Retry attempt ${retryCount + 1} for image ${index + 1}`);
+        return new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(downloadImage(url, index, retryCount + 1));
+          }, 1000 * retryCount); // wait for retryCount seconds before retrying
+        });
+      } else {
+        logToFile(`Max retries exceeded for image ${index + 1}`);
+      }
+    });
+}
 
 function downloadImages(imageUrls) {
-  imageUrls.forEach((url, index) => {
-    axios.get(url, { responseType: 'arraybuffer' })
-      .then(response => {
-        const imageType = url.split('.').pop();
-        const fileName = `image-${index + 1}`;
-        const filePath = path.join(imagesFolderPath, fileName + '.' + imageType);
-
-        // Delete old file with the same name but different extension if exists
-        const extensions = ['jpg', 'png', 'gif'];
-        extensions.forEach(ext => {
-          if (ext !== imageType) {
-            const oldFilePath = path.join(imagesFolderPath, fileName + '.' + ext);
-            fs.unlink(oldFilePath, (err) => {
-              if (err && err.code !== 'ENOENT') {
-                // 'ENOENT' means file doesn't exist, ignore that error
-                logToFile('Error deleting old image file: ' + err);
-              }
-            });
-          }
-        });
-
-        // Write the new file
-        fs.writeFile(filePath, Buffer.from(response.data), (error) => {
-          if (error) {
-            logToFile('Error writing image file: ' + error);
-          } else {
-            logToFile(`Image ${index + 1} saved as ${fileName}.${imageType}`);
-          }
-        });
-      })
-      .catch(error => logToFile('Error downloading image: ' + error));
-  });
+  const downloadPromises = imageUrls.map((url, index) => downloadImage(url, index));
+  Promise.allSettled(downloadPromises)
+    .then(() => logToFile('All images download attempts finished.'))
+    .catch(error => {
+      if (error instanceof AggregateError) {
+        // Log the individual errors
+        error.errors.forEach((err) => logToFile(err));
+      } else {
+        // Log any other type of error
+        logToFile(error);
+      }
+    });
 }
