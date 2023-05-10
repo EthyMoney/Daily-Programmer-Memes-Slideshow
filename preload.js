a357652eebbc68f8fa0ae425ba0e65fa82a081c1
Logan <ultimate360solutions@gmail.com>
2023-05-10 13:07:55 -0500

    ensuring images download before starting app

--- preload.js
+++ preload.js
@@ -1,10 +1,10 @@
 const { contextBridge, ipcRenderer } = require('electron');
 const logToFile = require('./logger');
+const fs = require('fs');
 
 contextBridge.exposeInMainWorld('electron', {
-  readDir: async (path) => {
-    return await ipcRenderer.invoke('read-dir', path);
-  },
+  readDir: (path) => ipcRenderer.invoke('read-dir', path),
+  exists: (path) => fs.existsSync(path),
   currentDir: __dirname,
   logToFile
 });
